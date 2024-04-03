fca4028387beba9ac31bdadec659d4aac224a270
serzhan29 <133089140+serzhan29@users.noreply.github.com>
2024-04-03 12:01:52 +0500

    CineFusion

---
+++ main/forms.py
@@ -0,0 +1,25 @@
+class AuthUserForm(AuthenticationForm, forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','password')
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+
+class RegisterUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','password')
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
